7e76a3e33ed46ba68fa09a1caa2d6f2808027d8f
afifdarmawan <darmawan_afif@ymail.com>
2017-02-21 00:08:58 +0700

    Update Member.java

--- src/task3exercise2/Member.java
+++ src/task3exercise2/Member.java
@@ -5,18 +5,13 @@ public class Member {
     // 1. Declare your private attributes here:
     // NIM : 1301154133
     // NAMA : Afif Darmawan
-    // Kelas : IF 39-07
-    //------------------- your code here-----------------------
+	// Kelas : IF 39-07
+
     private String name;
     private String specialization;
     private int projectWorked;
     //---------------------------------------------------------
     
-    // 2. Declare your 2 Constructor here:
-    // NIM : 1301154133
-    // NAMA : Afif Darmawan
-    // Kelas : IF 39-07
-    //------------------- your code here-----------------------
     public Member(String name){
         this.name = name;
     }
@@ -25,15 +20,7 @@ public class Member {
         this.specialization = special;
     }
     
-    //---------------------------------------------------------
-    
-
-    // 3. Declare your Setter and Getter method here:
-    // NIM : 1301154133
-    // NAMA : Afif Darmawan
-    // Kelas : IF 39-07
-    //------------------- your code here-----------------------
-    public int getProjectWorked() {
+	public int getProjectWorked() {
         return projectWorked;
     }
     public String getName() {
@@ -56,24 +43,11 @@ public class Member {
     public void setProjectWorked(int projectWorked) {
         this.projectWorked = projectWorked;
     }
-    
-
-    //---------------------------------------------------------
-    // 4. Create method toString() : String here:
-    // NIM : 1301154133
-    // NAMA : Afif Darmawan
-    // Kelas : IF 39-07
-    // returns String that contains member name, specialization,
-    // and number of project worked
-    //
-    // example : "Member Alex specialized in Java Programming, 
-    //            worked on 5 project(s)"
-    public String toString() {
-        //------------------- your code here-----------------------
+ 
+ public String toString() {
         return "Member "+name+" specialized in "+specialization+" Programming, worked on "+projectWorked+" project(s)";
         
         //---------------------------------------------------------
     }
 
 }
-
